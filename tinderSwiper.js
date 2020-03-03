371670226dbd11d00cb5d352672ea16e2f57b252
Fyre91 <ktpiyush91@gmail.com>
2020-03-03 20:44:24 +0530

    Updated for new UI changes

--- tinderSwiper.js
+++ tinderSwiper.js
@@ -7,31 +7,28 @@ function setTimeoutWrapper(cb, ms) {
     });
 }
 async function swiper() {
-    console.log(document.querySelector('.recCard__img'));
-    document.querySelector('.recCard__img').click();
+    const profileCard = document.querySelector('.recCard__info');
+    if (!profileCard) return;
+    profileCard.click();
     await setTimeoutWrapper(function() { console.log('waiting'); }, 500);
-    var carouselLength = document.querySelectorAll('.bullet').length;
-    var description = document.querySelector('.profileCard__bio span');
-    var descriptionLen = description ? description.innerHTML.trim().length : 0;
-    var rightSwiper = document.querySelectorAll('.pageButton')[1];
+    var carouselItems = document.querySelectorAll('.Expand.tappable-view .bullet');
+    var carouselLength = carouselItems.length;
+    var description = document.querySelector('.BreakWord');
+    var descriptionLen = description ? description.innerText.trim().length : 0;
 
     if (carouselLength === 0 && descriptionLen === 0) {
-        document.querySelector('.recsGamepad__button--dislike').click();
+        document.querySelector('[aria-label="Nope"]').click();
         return;
     }
-    while(carouselLength--) {
-        await setTimeoutWrapper(function() {
-            rightSwiper.click();
-        }, Math.floor(Math.random() * 700) + 300);
+    for (const image of carouselItems) {
+         await setTimeoutWrapper(function() {
+            image.click();
+        }, Math.floor(Math.random() * 700) + 800);   
     }
     await setTimeoutWrapper(function() { console.log('waiting'); }, 1000);
-    var profileCard = document.querySelector('.profileCard__card');
-    var cardHeight = profileCard.getBoundingClientRect().height;
-    profileCard.scrollTop = cardHeight;
-    await setTimeoutWrapper(function() { console.log('reading'); }, 1000);
     
 
-    document.querySelector('.recsGamepad__button--like').click();
+    document.querySelector('[aria-label="Like"]').click();
     console.log('End');
 }
 
@@ -39,3 +36,5 @@ async function tinderSwiper() {
     await setTimeoutWrapper(swiper, Math.floor(Math.random() * 2000) + 1000);
     tinderSwiper();
 }
+
+tinderSwiper();
